8d02311da60229f614327e419902521df7e45922
Onizuka893 <92974194+Onizuka893@users.noreply.github.com>
2023-09-25 16:48:01 +0200

    some more changes

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -4,10 +4,10 @@ const config = {
   github: {
     username: 'onizuka893', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
-    limit: 3, // How many projects to display.
+    limit: 5, // How many projects to display.
     exclude: {
       forks: true, // Forked projects will not be displayed if set to true.
-      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      projects: ['Extra-oefening-arrays', 'YoutubeApiTestMAUIBlazor'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
